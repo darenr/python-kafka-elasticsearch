e84d2a7d4332b29bf47a378fe8069bd7c0f37cb0
darenr <daren.race@gmail.com>
2015-09-04 15:33:30 -0700

    added iss tracker

--- avroconsumer.py
+++ avroconsumer.py
@@ -4,6 +4,7 @@ import avro.io
 import io
 import requests
 import json
+import sys
 
 topic = 'test'
  
@@ -12,11 +13,17 @@ consumer = KafkaConsumer(topic,
                          group_id='avro_group',
                          bootstrap_servers=['localhost:9092'])
  
-schema = avro.schema.parse(requests.get("http://localhost:8080/ingest/v1/get_avro_schema.json/" + topic).content)
-
-print schema
- 
+schema = None
 for msg in consumer:
+    if not schema:
+      r = requests.get("http://localhost:8080/ingest/v1/get_avro_schema.json/" + topic)
+      if not r.status_code == 200:
+        print "no schema"
+        sys.exit(-1)
+
+      schema = avro.schema.parse(r.json()['schema'])
+      print schema
+ 
     bytes_reader = io.BytesIO(msg.value)
     decoder = avro.io.BinaryDecoder(bytes_reader)
     reader = avro.io.DatumReader(schema)
---
+++ issproducer.py
@@ -0,0 +1,49 @@
+from kafka import SimpleProducer, KafkaClient
+from kafka.common import LeaderNotAvailableError
+import avro.schema
+from avro.io import DatumWriter
+import io
+import requests
+import sys
+import csv
+import json
+import time
+import datetime
+import re
+
+# To send messages synchronously
+kafka = KafkaClient('localhost:9092')
+producer = SimpleProducer(kafka)
+
+topic = 'iss'
+
+schema = {
+    "type": "record", "name": "iss",
+    "fields": [
+      {"name": "latlon", "type": "string"},
+      {"name": "timestamp", "type": "long"}
+    ]
+}
+
+avro_schema = avro.schema.parse(json.dumps(schema))
+
+# put the schema in the avro registry
+print requests.put("http://localhost:8080/ingest/v1/set_avro_schema.json/" + topic, data=json.dumps(schema))
+
+for x in range(10):
+  time.sleep(1)
+  j = requests.get("http://api.open-notify.org/iss-now.json").json()
+
+  m = { "timestamp": j['timestamp'],
+        "latlon": str(j['iss_position']['latitude']) \
+          + ','  \
+          + str(j['iss_position']['longitude']) }
+
+  print m
+  writer = avro.io.DatumWriter(avro_schema)
+  bytes_writer = io.BytesIO()
+  encoder = avro.io.BinaryEncoder(bytes_writer)
+    
+  writer.write(m, encoder)
+  raw_bytes = bytes_writer.getvalue()
+  producer.send_messages(topic, raw_bytes)
